dd79a5430f1de0dd152565c541bdd23b8f047662
khoitd1997 <khoidinhtrinh@gmail.com>
2018-09-01 01:40:10 -0700

    move things to better location and added comments about handler config

--- pc_controller/src/device_config.hpp
+++ pc_controller/src/device_config.hpp
@@ -5,11 +5,21 @@
 #include "power_ctrl_handler.hpp"
 #include "temp_sensor_handler.hpp"
 
-#define TOTAL_HANDLER 2
-
 #define SUBSCRIBE_TOPIC "/pcReq"
 #define PUBLISH_TOPIC "/pcRes"
 
+/****************Device Handler Config*****************/
+
+/**
+ * @brief change this if you use more or less handler
+ *
+ */
+#define TOTAL_HANDLER 2
+
+/**
+ * @brief change this function to customize the list of handler
+ *
+ */
 #define INITIALIZE_HANDLER()              \
   static PowerCtrlHandler  pwrController; \
   static TempSensorHandler tempHandler;   \
