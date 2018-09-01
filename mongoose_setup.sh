c7f7558708232c553e4a0ec107ae192927d74c6f
khoitd1997 <khoidinhtrinh@gmail.com>
2018-09-01 00:07:14 -0700

    change shell script structure based on new file layout

--- mongoose_setup.sh
+++ mongoose_setup.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 # used for setting up the dev board with necessary info
 
+#----------BUILD CONFIG VARIABLE SECTION-------------
+build_dir="pc_controller/" # change based on the devices you want to build
+
 #----------DEPENDENCY SECTION------------------------
-source credentials/mongoose_info.sh # get necessary info
+source ${build_dir}/credentials/mongoose_info.sh # get device info
+source system_info/system_info.sh # get system info
 set -e
 set -x
 
 #----------BUILD AND FLASH SECTION-------------------
+cd ${build_dir}
 mos build --arch ${arch} --clean
 mos flash
 mos wifi ${guest_wifi_ssid} ${gues_wifi_password}
--- utils_scripts/create_aws_key.sh
+++ utils_scripts/create_aws_key.sh
@@ -5,6 +5,6 @@ set -x
 set -e
 aws --region us-east-1 iot create-keys-and-certificate \
     --set-as-active \
-    --certificate-pem-outfile=fs/certificate.pem.crt \
-    --public-key-outfile=fs/public.pem.key \
-    --private-key-outfile=fs/private.pem.key
+    --certificate-pem-outfile=../fs/certificate.pem.crt \
+    --public-key-outfile=../fs/public.pem.key \
+    --private-key-outfile=../fs/private.pem.key
